3b18738c3ad6ad16bfc0eb4e09feb3707442394d
ddghost <31623047+ddghost@users.noreply.github.com>
2018-04-03 22:07:52 +0800

    Add files via upload

---
+++ unity hw2/Assets（太阳系，括号内删除）/script/旋转.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 旋转 : MonoBehaviour {
+	public GameObject mid;
+	public float speed;
+	public Vector3 angle;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (5f, 40f);
+		angle = new Vector3 (0  , 1 , Random.Range(-0.3f , 0.3f) );
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.RotateAround (mid.transform.position, angle, speed * Time.deltaTime);
+	}
+}
