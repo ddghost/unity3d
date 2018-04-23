a432200aaa833e689ab5bd695670e77607ffa119
ddghost <31623047+ddghost@users.noreply.github.com>
2018-04-23 17:34:05 +0800

    Add files via upload

---
+++ unity hw5/Assets/script/SSActionManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using baseCode;
+public class SSActionManager : MonoBehaviour {
+	protected Dictionary<int, SSAction> actions = new Dictionary<int, SSAction>();
+	protected List<SSAction> waitingAdd = new List<SSAction>();
+	protected List<int> waitingDelete = new List<int>();
+	protected roundController roundCtrl ;
+
+	void Start()
+	{
+		
+	}
+
+	public SSActionManager(){
+		roundCtrl = (roundController)Director.getInstance ().currentSceneController;
+
+	}
+
+	public void RunAction(GameObject gameobject, SSAction action, ISSActionCallback manager)
+	{
+		action.gameobject = gameobject;
+		action.transform = gameobject.transform;
+		action.callback = manager;
+		waitingAdd.Add(action);
+		action.Start();
+	}
+
+	public void reset(){
+		actions.Clear ();
+	}
+}
